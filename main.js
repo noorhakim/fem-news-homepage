131adcb16583f119c44f249ed002d54c43f03f4b
noorhakim <44545559+noorhakim@users.noreply.github.com>
2023-02-16 11:52:47 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const header = document.querySelector(".header");
+const menuToggle = document.querySelector(".menu-toggle");
+const menuClose = document.querySelector(".menu-close");
+const nav = document.querySelector(".nav");
+
+menuToggle.addEventListener("click", (e) => {
+  nav.classList.toggle("open");
+  header.toggleAttribute("data-overlay");
+  menuClose.toggleAttribute("data-visible");
+});
+
+menuClose.addEventListener("click", (e) => {
+  nav.classList.toggle("open");
+  header.toggleAttribute("data-overlay");
+  menuClose.toggleAttribute("data-visible");
+});
